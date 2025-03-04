e9e757ddb2c1b5597c69da6a8247eabad85b7e26
bisikeshan sahu <bisikeshan567@gmail.com>
2025-03-04 12:05:17 +0530

    12:04 04-03-2025 looping problem statement codes

---
+++ 04_Looping_Statements/04_Problem_Statement/code_06.c
@@ -0,0 +1,34 @@
+// Find the Product of Digits of a Number: Write a program to calculate and print the product of all digits in a given number using a loop.
+
+// Headers
+
+#include <stdio.h>
+
+int count; 
+int main()
+{
+    int num = 1202, Quotient = 0, Remainder = 0, result = 1, result_z = 0 ;  
+
+     while( num != 0  )
+     {
+        Quotient = num / 10; 
+        Remainder = num % 10;
+
+        if(Remainder == 0 )
+        {
+           result += Remainder ;
+        }
+        else
+        {
+          result *= Remainder ; 
+        }
+
+        num = Quotient; 
+     }
+
+     printf("result = %d\n",result);
+
+   
+
+    return 0; 
+}
