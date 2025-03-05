b7184fd90afa7bfb663eddc020942f731b2fee81
bisikeshan sahu <bisikeshan567@gmail.com>
2025-03-05 15:58:17 +0530

    3:58 PM 05-03-2025 array logic building codes

---
+++ 06_Array/03_Logic_Building/code_02_03.c
@@ -0,0 +1,32 @@
+// Headers
+#include <stdio.h>
+
+// Entry point function 
+int main()
+{
+	 float marks[5]; // declare an array to store marks of 5 students
+	 float sum = 0; // variable to store sum of marks 
+	 int le = 0; // loop variable 
+
+	 // prompt user to enter marks for 5 students
+	 printf("Enter marks for 5 students:\n"); 
+
+	 // loop to take input from the user
+	 for(le = 0; le < 5; le++)
+	 {
+	 	scanf("%f",&marks[le]);
+	 }
+
+	 // loop to calculate the sum of marks
+
+	 for(le = 0; le < 5; le++)
+	 {
+	 	sum += marks[le]; 
+	 }
+
+	 // calculate and display the average marks
+
+	 printf("Average Marks: %.2f\n",sum/5); 
+
+	 return 0; 	
+}
